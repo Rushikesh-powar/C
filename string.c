96078ddede8cdd2659563bfe4c3a9582f2344ffc
Rushi_99 <powarrushi10@gmail.com>
2019-09-28 12:00:31 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  char a[10];
+  int i,j;
+
+  printf("Enter name: ");
+  scanf(" %s", a);
+
+  printf("The length of string is: ");
+  for(i=0; a[i] !='\0';i++)
+  
+   j=i;
+   printf("%d\n",j);
+  
+return 0;
+}
