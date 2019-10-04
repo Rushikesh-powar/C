e74289eb22c66bc53713e8ad5da6bf666c1774f3
Rushi_99 <powarrushi10@gmail.com>
2019-10-04 10:31:34 +0530

    Update and rename gcd.c to Finding_GCD_of_Numbers.c

--- Finding_GCD_of_Numbers.c
+++ Finding_GCD_of_Numbers.c
@@ -1,66 +1,43 @@
 #include<stdio.h>
 
-
 int gcd(int,int);
 
 int main()
 {
-
   int a,b;
 
   printf("Enter two Number's to Find GCD: ");
-
   scanf("%d %d",&a,&b);
-
+	
   printf("The GCD of %d and %d is: %d\n",a,b,gcd(a,b));
-
-
+	
   return 0;
-
 }
 
 int gcd(int x,int y)
-
 {
   int i,j;
 
   if(x>y)
-
   {
-
       for(i=1; i<=x;i++)
-
       {
-
- 
        if(y%i==x%i)
- 
         {
-
           j=i;
 
 	  printf(" %d ");
 
 	  return j;
-
         }
-  
   }
-
-
  }
-
  if(y>x)
-
  {
- 
+	 
  for(i=1; i<=y;i++)
-
   {
-
-
       if(y%i==x%i)
- 
         {
 
           j=i;
@@ -68,11 +45,7 @@ int gcd(int x,int y)
 	  printf(" %d ");
 
 	  return j;
-		
         }
- 
   }
-
  }
-
 }
