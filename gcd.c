6df00821f08d18f7cee73d39f9d5196bb875cee7
Rushi_99 <powarrushi10@gmail.com>
2019-10-04 10:27:13 +0530

    Add files via upload

---
+++ gcd.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+
+
+int gcd(int,int);
+
+int main()
+{
+
+  int a,b;
+
+  printf("Enter two Number's to Find GCD: ");
+
+  scanf("%d %d",&a,&b);
+
+  printf("The GCD of %d and %d is: %d\n",a,b,gcd(a,b));
+
+
+  return 0;
+
+}
+
+int gcd(int x,int y)
+
+{
+  int i,j;
+
+  if(x>y)
+
+  {
+
+      for(i=1; i<=x;i++)
+
+      {
+
+ 
+       if(y%i==x%i)
+ 
+        {
+
+          j=i;
+
+	  printf(" %d ");
+
+	  return j;
+
+        }
+  
+  }
+
+
+ }
+
+ if(y>x)
+
+ {
+ 
+ for(i=1; i<=y;i++)
+
+  {
+
+
+      if(y%i==x%i)
+ 
+        {
+
+          j=i;
+
+	  printf(" %d ");
+
+	  return j;
+		
+        }
+ 
+  }
+
+ }
+
+}
