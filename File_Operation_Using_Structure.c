714364369a6e285185eac439133f9c1abc3dce67
Rushi_99 <powarrushi10@gmail.com>
2019-12-04 20:05:21 +0530

    Update and rename main.c to File_Operation_Using_Structure.c

--- File_Operation_Using_Structure.c
+++ File_Operation_Using_Structure.c
@@ -14,7 +14,7 @@ int main()
     struct data std;
     FILE *fptr;
 
-    fptr=fopen("G:\\C_program.txt","w");
+    fptr=fopen("G:\\C_program.txt","w");    ////you must create an .txt file before runnig this programm at that location in c drive.
 
     if(fptr == '\0')
     {
@@ -24,7 +24,7 @@ int main()
     printf("Enter The Student Data: ");
     scanf(" %s %s %d",&std.name,&std.clas,&std.rollno);
 
-    fprintf(fptr," %s\n",std.name);
+    fprintf(fptr," %s\n",std.name);             ////line 27 to line 29 will print the user defined data from the input.
     fprintf(fptr," %s\n",std.clas);
     fprintf(fptr," %d\n",std.rollno);
 
